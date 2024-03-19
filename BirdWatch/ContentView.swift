416bdfa54492615efabe934e3093632a6de367ee
Vin Bui <vdb23@cornell.edu>
2024-03-18 23:12:16 -0400

    Finish refactoring

--- BirdWatch/ContentView.swift
+++ BirdWatch/ContentView.swift
@@ -10,34 +10,7 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        List(birds, id: \.self) { bird in
-            birdInfoRow(bird)
-        }
-    }
-
-    private func birdInfoRow(_ bird: Bird) -> some View {
-        HStack {
-            Text(String(bird.count))
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.trailing, 30)
-
-            VStack(alignment: .leading) {
-                Text(bird.name)
-                    .fontWeight(.bold)
-
-                Text(bird.location)
-                    .fontWeight(.medium)
-                    .italic()
-            }
-            .font(.title2)
-
-            Spacer()
-
-            Image(bird.image)
-                .resizable()
-                .frame(width: 100, height: 100)
-        }
+        BirdListView()
     }
 
 }
